b20c8d7b605a17fcdafb8a55cb7e4018220af243
yhupe <hannes.pickel@outlook.com>
2025-06-12 14:12:47 +0200

    linking the products_router endpoints to the main.py file

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from dotenv import load_dotenv
+
+from api.routes.products import product_router
+
 load_dotenv()
 
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 
 # Import of router
-from api.routes import employees, message_logs, auth
+from api.routes import employees, message_logs, auth, products
 
 app = FastAPI()
 
@@ -14,6 +17,9 @@ app.include_router(employees.employees_router)
 
 # linking the message_log_router with main.py
 app.include_router(message_logs.message_log_router)
+
+# linking the products_router with main.py
+app.include_router(products.product_router)
 
 # linking the auth_router with main.py
 templates = Jinja2Templates(directory="templates")
