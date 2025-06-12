70b7a67b45bcee8cf413e4f07fbf863f829d1338
yhupe <hannes.pickel@outlook.com>
2025-06-12 14:09:15 +0200

    creating service layer for handling Products database table CRUD operations, creating product_router endpoints that are calling those services respectively.

---
+++ api/routes/products.py
@@ -0,0 +1,210 @@
+# Import of necessary parts of FastAPI
+from fastapi import APIRouter, Depends, HTTPException, status, Response
+
+# Import of or_ module as a filtering condition to avoid using '|'
+from sqlalchemy import or_
+
+# Import of SQLAlchemy Session (for type hints)
+from sqlalchemy.orm import Session
+
+from api.schemas import ProductUpdate, ProductCreate
+# Import of SQLAlchemy ORM models
+from database import models
+
+# Import of Pydantic schemas
+from api import schemas
+
+# Import of database dependency
+from database.database import get_db
+
+# Import of EmployeeService
+from services.products_service import ProductService, get_product_service
+
+from uuid import UUID
+from typing import List, Optional
+
+
+# Creates APIRouter instance
+product_router = APIRouter(
+    prefix="/products",
+    tags=["products"],
+)
+
+
+@product_router.post("/", response_model=schemas.Product, status_code=status.HTTP_201_CREATED)
+def create_product(
+        product_data: ProductCreate,
+        product_service: ProductService = Depends(get_product_service)
+):
+    """ Endpoint to create a new product.
+
+        Args:
+            product_data (schemas.ProductCreate): The Pydantic model containing the details
+                for a new product.
+            product_service (ProductService): The injected ProductService instance.
+
+        Returns: db_product: The newly created product object incl. the automatically generated
+            ID and timestamps.
+
+        Raises:
+            HTTPException: If the provided product name is
+                already in the database (HTTP 400 Bad Request).
+        """
+
+    try:
+        db_product = product_service.create_product(product_data=product_data)  # calling the service
+        return db_product
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
+
+
+@product_router.get("/{product_id}", response_model=schemas.Product, status_code=status.HTTP_200_OK)
+def get_product_by_id(
+    product_id: UUID,
+    product_service: ProductService = Depends(get_product_service)
+):
+    """ Endpoint to get a product by ID.
+
+    Args:
+        product_id (UUID): A unique identifier in UUID format.
+        product_service (ProductService): The injected ProductService instance.
+
+    Returns: db_product: The employee object searched for.
+
+    Raises: HTTPException: If the product cannot be found by the passed ID (HTTP 404 Not Found)
+
+    """
+
+    db_product = product_service.get_product_by_id(product_id=product_id)
+    if not db_product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+
+    return db_product
+
+
+@product_router.get("/{product_name}", response_model=schemas.Product, status_code=status.HTTP_200_OK)
+def get_product_by_name(
+    product_name: str,
+    product_service: ProductService = Depends(get_product_service)
+):
+    """ Endpoint to get a product by name.
+
+    Args:
+        product_name (str): A products name in str type.
+        product_service (ProductService): The injected ProductService instance.
+
+    Returns: db_product: The product object searched for.
+
+    Raises: HTTPException: If the product cannot be found by the passed name (HTTP 404 Not Found)
+
+    """
+
+    db_product = product_service.get_all_products(name_query=product_name)
+    if not db_product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+
+    return db_product
+
+
+@product_router.get("/all", response_model=List[schemas.Employee], status_code=status.HTTP_200_OK)
+def get_all_products(
+    product_service: ProductService = Depends(get_product_service)
+):
+    """ Endpoint to get all products.
+
+    Args:
+        product_service (ProductService): The injected ProductService instance.
+
+    Returns: db_product: The list of all product objects.
+
+    Raises: HTTPException: If the product cannot be found by the passed name (HTTP 404 Not Found)
+
+    """
+
+    db_product = product_service.get_all_products
+    if not db_product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No products found")
+
+    return db_product
+
+
+@product_router.patch("/{product_id}", response_model=schemas.Product, status_code=status.HTTP_200_OK)
+def update_product(
+        product_id: UUID,
+        product_update_data: ProductUpdate,
+        product_service: ProductService = Depends(get_product_service)
+):
+    """ Endpoint to update at least one field of an existing product.
+
+    Args:
+        product_id (UUID): A unique identifier in UUID format.
+        product_update_data (ProductUpdate): An ProductUpdate object containing the updates.
+        product_service (ProductService): The injected ProductService instance.
+
+    Returns: db_product: The updated Product object.
+
+    Raises:
+        HTTPException:
+            - 404 Not found: If the ID searched after does not match with a
+            product from the database.
+            - 400 Bad request: If there is a database error like unique constraint violation
+            - 422 Unprocessable Entity: If the input data is invalid (Pydantic)
+    """
+
+    try:
+        db_product = product_service.update_product(
+            product_id=product_id,
+            product_update_data=product_update_data
+        )
+        return db_product
+
+    except ValueError as e:
+
+        if "Product not found" in str(e):
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=str(e)
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=str(e)
+            )
+
+
+@product_router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_product_by_id(
+        product_id: UUID,
+        product_service: ProductService = Depends(get_product_service)
+):
+    """ Endpoint to delete a product by id.
+
+    Args:
+        product_id (UUID): A unique identifier in UUID format.
+        product_service (ProductService): The injected ProductService instance.
+
+    Returns: Response(status_code=status.HTTP_204_NO_CONTENT): Returns a response without body.
+
+    Raises:
+        HTTPException: If the product cannot be found by the passed ID (HTTP 404 Not Found)
+    """
+
+    try:
+        product_service.delete_product(product_id=product_id)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    except ValueError as e:
+        if "Product not found" in str(e):
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=str(e)
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=str(e)
+            )
+
---
+++ services/products_service.py
@@ -0,0 +1,131 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import or_
+from uuid import UUID
+from typing import List, Optional
+
+from database import models
+from api.schemas import ProductCreate, ProductUpdate
+from database.database import get_db
+from fastapi import Depends, HTTPException
+
+class ProductService:
+    def __init__(self, db: Session):
+        """
+        Initializes the ProductService with a db-session.
+        """
+        self.db = db
+
+
+    def create_product(
+        self,
+        product_data: ProductCreate
+    ) -> models.Product:
+        """
+        Creates a new product in the database.
+        """
+
+        # Check whether an employee with the exact name already exists
+        db_product = self.db.query(models.Product).filter(
+            or_(
+                models.Product.name == product_data.name,
+            )
+        ).first()
+
+        if db_product:
+            raise ValueError("Product with this exact name already exists.")
+
+        new_product = models.Product(
+            name=product_data.name,
+            description=product_data.description,
+            length=product_data.length,
+            height=product_data.height,
+            width=product_data.width,
+            weight=product_data.weight,
+            image_url=product_data.image_url,
+            price=product_data.price,
+            stock_quantity=product_data.stock_quantity,
+            is_active=product_data.is_active,
+            notes=product_data.notes
+        )
+
+        self.db.add(new_product)
+        self.db.commit()
+        self.db.refresh(new_product)
+        return new_product
+
+    def get_product_by_id(self, product_id: UUID) -> Optional[models.Product]:
+        """
+        Retrieves a product by ID.
+        """
+
+        return self.db.query(models.Product).filter(models.Product.id == product_id).first()
+
+    def get_all_products(self, name_query: Optional[str] = None) -> List[models.Product]:
+        """
+        Retrieves a list of products, optionally filtered by name.
+        """
+
+        query = self.db.query(models.Product)
+
+        if name_query:
+            query = query.filter(models.Product.name.ilike(f"%{name_query}%"))
+
+        products = query.all()
+        return products
+
+
+    def update_product(
+        self,
+        product_id: UUID,
+        product_update_data: ProductUpdate
+    ) -> Optional[models.Product]:
+        """
+        Updates an existing product's fields.
+        Handles unique constraint violations.
+        """
+
+        db_product = self.get_product_by_id(product_id)
+
+        if not db_product:
+            raise ValueError("Product not found")
+
+        update_data = product_update_data.model_dump(exclude_unset=True)
+
+        for key, value in update_data.items():
+            setattr(db_product, key, value)
+
+        try:
+            self.db.add(db_product)
+            self.db.commit()
+            self.db.refresh(db_product)
+            return db_product
+
+        except Exception as e:
+            self.db.rollback()
+            error_detail = str(e.orig) if hasattr(e, 'orig') else str(e)
+            raise ValueError(f"Database error updating product: {error_detail}")
+
+
+    def delete_product(self, product_id: UUID) -> bool:
+        """
+        Deletes an employee by ID.
+        Returns True if deleted, False if not found.
+        """
+
+        db_product = self.get_product_by_id(product_id)
+
+        if not db_product:
+            raise ValueError("Product not found")
+
+        self.db.delete(db_product)
+        self.db.commit()
+        return True
+
+# Dependency for FastAPI-Router
+# Function is used by FastAPI as dependency to inject a service instance
+def get_product_service(db: Session = Depends(get_db)) -> ProductService:
+    """
+    Dependency that returns an instance of EmployeeService
+    """
+
+    return ProductService(db)
